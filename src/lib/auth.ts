1e1aeab53ff79a17299407c5ecce33c4e99daabf
yankaique <yan.contato.dev@gmail.com>
2025-01-27 21:05:02 -0300

    feat: add nextauth config

---
+++ src/lib/auth.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth"
+import prisma from "./db"
+import NodeMailer from "next-auth/providers/nodemailer"
+import Google from "next-auth/providers/google"
+import Facebook from "next-auth/providers/facebook"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    NodeMailer({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+    Google,
+    Facebook,
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    newUser: "/auth/signup",
+    error: "/auth/error",
+  },
+  callbacks: {
+   async session({ session }) {
+     return session
+   } 
+  }
+})

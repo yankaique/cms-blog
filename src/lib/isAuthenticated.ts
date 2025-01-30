0ea2fa04564fda5c914e362f40d00600b1cc46e8
yankaique <yan.contato.dev@gmail.com>
2025-01-30 08:47:39 -0300

    fix: navigation import error

--- src/lib/isAuthenticated.ts
+++ src/lib/isAuthenticated.ts
@@ -1,10 +1,11 @@
 import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
+import { redirect } from "@/lib/navigation";
+import { intl } from "@/config/intl";
 
 export const isAuthenticated = async () => {
     const session = await auth()
-    
-    if(!session?.user) redirect("/auth/signin")
+
+    if(!session?.user) redirect({ href: "/auth/signin", locale: intl.defaultLocale })
 
     return session;
 };
--- src/lib/isNotAuthenticated.ts
+++ src/lib/isNotAuthenticated.ts
@@ -1,8 +1,9 @@
 import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
+import { redirect } from "@/lib/navigation";
+import { intl } from "@/config/intl";
 
 export const isNotAuthenticated = async () => {
     const session = await auth()
     
-    if(session?.user) redirect("/")
+    if(session?.user) redirect({ href: "/", locale: intl.defaultLocale })
 };

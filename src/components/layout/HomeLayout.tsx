390eec51ad8f1eff17ce662511335be78b586b29
Yan <yan.contato.dev@gmail.com>
2025-02-01 20:02:01 -0300

    feat: create home layout

---
+++ src/components/layout/HomeLayout.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { Link } from "@/lib/navigation"
+import Image from "next/image"
+import Logo from "@/assets/imgs/logo.svg"
+import { Layout } from "antd"
+import { LocaleDropdown } from "./LocaleDropdown"
+import { ToggleTheme } from "./ToggleTheme"
+const { Header, Content } = Layout
+
+const HomeLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return (
+        <Layout className="h-screen overflow-hidden">
+            <Layout className="dark:bg-slate-900">
+                <Header className="
+                    flex 
+                    justify-between 
+                    items-center 
+                    gap-4 
+                    xl:px-40 
+                    bg-white 
+                    dark:bg-slate-950
+                    border-b
+                    border-slate-200
+                    dark:border-b-zinc-800
+                ">
+                    <Link href="/">
+                        <Image src={Logo} width={150} alt="logo" priority />
+                    </Link>
+                    <div className="flex items-center gap-4">
+                        <LocaleDropdown />
+                        <ToggleTheme />
+                    </div>
+                </Header>
+                <Content className="
+                    flex 
+                    items-center 
+                    justify-center 
+                    overflow-auto
+                    bg-white
+                    dark:bg-slate-950
+                ">
+                    {children}
+                </Content>
+            </Layout>
+        </Layout>
+    )
+}

ac3c7e9c8e8995debab903f0621828b3a6ccf2e2
yankaique <yan.contato.dev@gmail.com>
2025-02-06 07:41:19 -0300

    feat: create verify email page

---
+++ src/components/pages/auth/VerifyEmail.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { Link } from "@/lib/navigation"
+import { InfoCircleOutlined } from "@ant-design/icons"
+import { Button, Result, theme } from "antd"
+import { useTranslations } from "next-intl"
+
+export const VerifyEmail = () => {
+    const { token: { colorPrimary } } = theme.useToken()
+    const translation = useTranslations("VerifyEmailPage")
+    return (
+        <Result 
+            status="info" 
+            icon={<InfoCircleOutlined style={{ color: colorPrimary }} />} 
+            title={translation("title")}
+            subTitle={translation("sub_title")}
+            className="max-w-3xl"
+            extra={
+                <Link href="/auth/signin">
+                    <Button type="primary">
+                        {translation("btn_label")}
+                    </Button>
+                </Link>
+            }
+        />
+    )
+}
